5a00bac6b4aa558a9af1679d2441aca66815fbc4
José de Jesús Rosas Núñez <125505426+josern01@users.noreply.github.com>
2023-02-28 19:40:54 -0600

    Add files via upload

---
+++ mx.uaemex.dto2/src/mx/uaemex/test2/TestArtistas.java
@@ -0,0 +1,48 @@
+package mx.uaemex.test2;
+
+import java.util.ArrayList;
+import java.util.List;
+import mx.uaemex.dto2.*;
+
+public class TestArtistas {
+
+    public static void main(String[] args) {
+
+        Artista Artista1 = new Artista();
+        Artista1.setNombreArtista("PostMalone");
+        Artista1.setNombre("Austin");
+        Artista1.setApellidoPaterno("Richard");
+        Artista1.setApellidpMaterno("Post");
+        Artista1.setId(2);
+
+        Cancion cancion1 = new Cancion();
+        cancion1.setCompositor("PostMalone");
+        cancion1.setFechalanzamineto(2018);
+        cancion1.setGenero("Trap");
+        cancion1.setNombre("RockStar");
+        cancion1.setId(1);
+
+       
+
+        Album album1 = new Album();
+        album1.setId(1);
+        album1.setNombre("Beerbongs & Bentleys");
+        album1.setFechalanzamineto(2018);
+
+        List<Artista> listaArtistas = new ArrayList<>();
+        listaArtistas.add(Artista1);
+
+        List<Persona> listaPersona = new ArrayList<>();
+        listaPersona.add(Artista1);
+
+        List<Cancion> listacancion = new ArrayList<>();
+        listacancion.add(cancion1);
+
+       
+
+        System.out.println(listacancion);
+        System.out.println(listaPersona);
+        System.out.println(album1);
+    }
+
+}
